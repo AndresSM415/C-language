7d4afba1b0e309164303bf74a23028ce587aa945
AndresSM415 <78566932+AndresSM415@users.noreply.github.com>
2021-02-25 14:06:07 -0600

    Update homewrok1.c

--- homework/homewrok1.c
+++ homework/homewrok1.c
@@ -3,10 +3,9 @@
 #define NCHAR 130
 #define COMMANDTYPES 4
 
-int c, i = 0;
-int nc = 0; //new command
-int state = COMMANDTYPES;
-int ln[NCHAR];//line
+int c, i = 0, ln[NCHAR];//(),(line index), (current line)
+int ip, g;// (i position, since command was found), (counter of characters into the command)
+int state = COMMANDTYPES;//To identify if there is a command to work with
 int incommand[COMMANDTYPES][33] = {//[row][column]
                         {'<','h','1','>'},
                         {'<','s','t','r','o','n','g','>'},
@@ -19,18 +18,16 @@ int outcommand[COMMANDTYPES][10] = {//[row][column]
                         {'<','/','p','>'},
                         {'/','s','c','r','i','p','t','>'},
                        };
-int queue[5];
-int iqueue = 0;//index queue
-int ip, g;// i position, counter of character into the command, state of command found
-
 void println();
 int newCommand();
 
 int main()
 {
+    int queue[5], iqueue = 0;//(check how many commands are waiting to be closed), (index queue)
+    int nc = 0; //new command
     char *filename = "C:\\Users\\andre\\Downloads\\locura.txt";
     FILE *fptr = fopen(filename, "r");//"w" if you want to edit a file (Minhaj A.)
-    //
+    
     while((c = getc(fptr)) != EOF)
     {
         ln[i] = c;
@@ -60,12 +57,10 @@ int main()
         else if((queue[iqueue-1] == 0 || queue[iqueue-1] == 1) && iqueue > 0 && (c == ' ' || c == '\t'))// comandos <h1> y strong
             ln[i] = '_';
         else if(queue[iqueue-1] == 3 && iqueue > 0)
-        {
-            while((c = getc(fptr)) != EOF){
+            while((c = getc(fptr)) != EOF)
                 if(c == '<')
                 {
-                    for(g = 0; (g < 8) && ((c = getc(fptr)) == outcommand[3][g]); g++){
-                    }
+                    for(g = 0; (g < 8) && ((c = getc(fptr)) == outcommand[3][g]); g++){}
                     if(g == 8)
                     {
                         iqueue--;
@@ -73,13 +68,10 @@ int main()
                         break;
                     }
                 }
-            }
-        }
         if(nc == 1)//si se ha abierto un comando guarda el numero de caracteres que tiene
         {
             ip++;
             ln[i] = tolower(c);
-
         }
         else if(c == '\n')
         {
@@ -109,7 +101,6 @@ int newCommand()
     }
     return state;
 }
-
 int oldCommand()
 {
     state = COMMANDTYPES;
@@ -125,10 +116,8 @@ int oldCommand()
     return state;
 }
 void println()
-{
     for(int e = 0; i-e > 0; e++)//escribe el texto ya modificado
         printf("%c", ln[e]);
-}
 /*ANSI C syntax gotten by Satu Elisa Schaeffer 	elisa.schaeffer@gmail.com
 opening a file code was gotten by Minhaj Ansari in this video: https://www.youtube.com/watch?v=G5A04QCi7e4&ab_channel=MinhajAnsari
 */
