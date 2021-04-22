497162a7013685479949168681b203468ff094f6
AndresSM415 <78566932+AndresSM415@users.noreply.github.com>
2021-04-22 14:01:24 -0500

    Create main.c

---
+++ homework/homework5/main.c
@@ -0,0 +1,66 @@
+#include "minesweeper.h"
+int MI = 10;
+
+int main()
+{
+    while(1)
+    {
+    char** mapa;
+    int** minas;
+
+    minas = (int**)malloc(sizeof(int*)*MI);
+    mapa = (int**)malloc(sizeof(int*)*MI);
+    for(int j = 0; j < MI; j++)
+    {
+        minas[j] = (int*)malloc(sizeof(int)*MI);
+        mapa[j] = (int*)malloc(sizeof(int)*MI);
+        for(int i = 0; i < MI; i++)
+        {
+            minas[j][i] = 0;
+            mapa[j][i] = '#';
+        }
+    }
+    for(int j = 0; j < MI; j++)
+    {
+        for(int i = 0; i < MI; i++)
+        {
+            minas[j][i] = 0;
+            mapa[j][i] = '#';
+        }
+    }
+
+    printf("cuantas minas quieres?: \n");
+    int c, noMinas = 0;
+    while(isdigit(c = getchar()))
+        noMinas = noMinas*10 + c - '0';
+    for(int i = 0; i <= noMinas; i++)
+        minas[rand()%10][rand()%10] = 9;
+
+    cuentaMinas(minas);
+    while(1)
+    {
+    int j, i, estado, coordenadas;
+    while((coordenadas =coordenada(&j, &i)) == -1){}
+    if((estado = muestraPos(minas, mapa, j, i, coordenadas)) == -1)
+    {
+        printf("perdiste!\n");
+        break;
+    }
+    else if(estado == 1)
+    {
+        printf("ganaste!\n");
+        break;
+    }
+
+    imprimeMapa(mapa);
+    }
+    for(int j = 0; j < MI; j++)
+    {
+        free(minas[j]);
+        free(mapa[j]);
+    }
+    free(minas);
+    free(mapa);
+    }
+    return 0;
+}
