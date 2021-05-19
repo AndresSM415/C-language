fa7bee78a344895a30ee2f99d02278d56f67dde2
AndresSM415 <78566932+AndresSM415@users.noreply.github.com>
2021-05-19 06:08:24 -0500

    Create my2048.h

---
+++ homework/FinalProject/my2048.h
@@ -0,0 +1,12 @@
+#ifndef MY2048_H_INCLUDED
+#define MY2048_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+void printMat(int**, int);
+void startRand(int**);
+int moveMatriz(int**);
+
+#endif // MY2048_H_INCLUDED
